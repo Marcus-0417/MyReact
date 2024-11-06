2a9e370075870999435589c86bab4b79d9eb413a
vicqq00qqq <vicqq00qqq@gmail.com>
2024-11-06 11:49:41 +0800

    update

---
+++ my-react-app1/src/App.jsx
@@ -0,0 +1,20 @@
+
+function MyComponent() {
+  return (
+    /* 空標籤：Fragment */
+    <>
+    <h1>我是外來種</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sint animi amet, repellendus et explicabo possimus ipsa quas dolores sed. Libero numquam mollitia a in? Minima magni optio velit sint.</p>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  )
+}
+
+export default App

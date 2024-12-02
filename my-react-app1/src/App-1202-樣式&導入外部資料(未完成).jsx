df79e06e86196b0eee09f22e7c77eff4e6d82261
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-02 12:25:57 +0800

    update

---
+++ my-react-app1/src/App-1202-樣式&導入外部資料(未完成).jsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react"
+import axios from "axios"
+import { IoUmbrella } from "react-icons/io5";
+
+
+function App() {
+
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get('https://raw.githubusercontent.com/Marcus-0417/MyReact/refs/heads/main/F-C0032-001.json');
+      const {locations} = data.data.cwbopendata.dataset;
+      console.log(locations);
+    })
+  }, [])
+
+  return (
+
+    <>
+      <div className="wrap">
+        <h2>36小時天氣預報</h2>
+        {/* 一列兩欄 */}
+        <div className="row">
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card"></div>
+            {/* 標題 */}
+            <div className="title">台北市</div>
+            {/* 內容 */}
+            <div className="content">
+              {/* 一列三欄 */}
+              <div className="row2">
+                {/* 第一欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第二欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第三欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card"></div>
+            {/* 標題 */}
+            <div className="title">台北市</div>
+            {/* 內容 */}
+            <div className="content">
+              {/* 一列三欄 */}
+              <div className="row2">
+                {/* 第一欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第二欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+                {/* 第三欄 */}
+                <div className="col2">
+                  <p>2日</p>
+                  <p>上午6:00<br />~<br />下午6:00</p>
+                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
+                  <p>晴時多雲</p>
+                  <p><IoUmbrella />10%</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+
+  )
+}
+
+export default App
--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,79 +1,32 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 function App() {
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      const data = await axios.get('./F-C0032-001.json');
-      const {locations} = data.data.cwbopendata.dataset;
-      console.log(locations);
+    const fetchData = async () => {
+      try {
+        // 使用 raw GitHub 鏈接
+        const response = await axios.get('https://raw.githubusercontent.com/Marcus-0417/MyReact/refs/heads/main/F-C0032-001.json');
+        
+        // 根據 JSON 結構調整路徑
+        const locationsData = response.data.cwbopendata.dataset.locations;
+        
+        console.log(locationsData);
+        setLocations(locationsData);
+      } catch (error) {
+        console.error("獲取數據時出錯:", error);
+      }
+    };
 
-    })
+    fetchData();
   }, [])
 
   return (
-
     <>
-      <h2>36小時天氣預報</h2>
-      <div id="listBox">
-        <div id="city">
-        <h3 className="cityName">台北市</h3>
-          <div id="outer">
-            <div className="inner">
-              <h3>2日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <img src="../public/weatherIcon/晴時多雲.svg" alt="" />
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-            <div className="inner">
-              <h3>2日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <figure><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></figure>
-              <p>多雲時晴</p>
-              <p>10%</p>
-            </div>
-            <div className="inner">
-              <h3>3日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <figure><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></figure>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-          </div>
-        </div>
-        <div id="city">
-          <h3 className="cityName">新北市</h3>
-          <div id="outer">
-            <div>
-              <h3>2日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <img src="../public/weatherIcon/晴時多雲.svg" alt="" />
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-            <div>
-              <h3>2日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <figure><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></figure>
-              <p>多雲時晴</p>
-              <p>10%</p>
-            </div>
-            <div>
-              <h3>3日</h3>
-              <p>上午6:00<br />~<br />下午6:00</p>
-              <figure><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></figure>
-              <p>晴時多雲</p>
-              <p>10%</p>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
+      {/* 可以在這裡渲染 locations 數據 */}
     </>
-
   )
 }
 

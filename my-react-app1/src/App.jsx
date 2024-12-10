66e6133351780aa1c4564bfae9325165ca4f0acd
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-10 11:58:19 +0800

    update

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,104 +1,13 @@
-import { useEffect } from "react"
-import axios from "axios"
-import { IoUmbrella } from "react-icons/io5";
+import $ from 'jquery'
 
-
-function App() {
-
-  useEffect(() => {
-    (async () => {
-      const data = await axios.get('');
-      const {locations} = data.data.cwbopendata.dataset;
-      console.log(locations);
-    })
-  }, [])
+export default function App() {
 
   return (
-
     <>
-      <div className="wrap">
-        <h2>36小時天氣預報</h2>
-        {/* 一列兩欄 */}
-        <div className="row">
-          <div className="col">
-            {/* 卡片樣式 */}
-            <div className="card"></div>
-            {/* 標題 */}
-            <div className="title">台北市</div>
-            {/* 內容 */}
-            <div className="content">
-              {/* 一列三欄 */}
-              <div className="row2">
-                {/* 第一欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-                {/* 第二欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-                {/* 第三欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            {/* 卡片樣式 */}
-            <div className="card"></div>
-            {/* 標題 */}
-            <div className="title">台北市</div>
-            {/* 內容 */}
-            <div className="content">
-              {/* 一列三欄 */}
-              <div className="row2">
-                {/* 第一欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-                {/* 第二欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-                {/* 第三欄 */}
-                <div className="col2">
-                  <p>2日</p>
-                  <p>上午6:00<br />~<br />下午6:00</p>
-                  <p><img src="./public/weatherIcon/晴時多雲.svg" alt="" /></p>
-                  <p>晴時多雲</p>
-                  <p><IoUmbrella />10%</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <h2>jquery</h2>
+        <button onClick={() => alert($('h2').text())}>click</button>
       </div>
-
     </>
-
   )
 }
-
-export default App

0fe5310de92df342c595fbe629c3bd4eac3ec7e2
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-17 11:14:10 +0800

    update

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,12 +1,36 @@
+import { useRef } from "react"
 
+export default function App() {
 
-function App() {
-return (
+  const greenRef = useRef(null);
+  const yellowRef = useRef(null);
 
-<>
-</>
+  const goGreen = () => {
+    // greenRef.current.scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({
+      behavior: 'smooth',
+      top: greenRef.current.offsetTop
+    })
+  }
+  const goYellow = () => {
+    // yellowRef.current.scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({
+      behavior: 'smooth',
+      top: yellowRef.current.offsetTop
+    })
+  }
 
-)
+  return (
+    <>
+      {/* offsetTop */}
+      <div ref={yellowRef} style={{ height: '100vh', backgroundColor: 'yellow', }}>
+        <button onClick={goGreen}>green</button>
+      </div>
+
+      <div ref={greenRef} style={{ height: '100vh', backgroundColor: 'green', }}>
+        <button onClick={goYellow}>yellow</button>
+      </div>
+
+    </>
+  )
 }
-
-export default App
